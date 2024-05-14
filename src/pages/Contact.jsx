09ab9393d26508a3a7e7c181f8be6792b3a61e85
Pranjal-gupta-2002 <123636190+Pranjal-gupta-2002@users.noreply.github.com>
2024-05-14 16:50:58 +0530

    emailJs code is added in contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,26 +4,86 @@ import { HiMail } from "react-icons/hi";
 import Button from "./Button";
 import "./Contact.css";
 import bg from "../components/assets/images/image2.png";
-import { useState } from "react";
+import React, { useRef, useState } from "react";
+import emailjs from "@emailjs/browser";
 import ContactHeader from "./ContactHeader";
 
 export const Contact = () => {
-  const [name, setName] = useState(undefined);
-  const [email, setEmail] = useState(undefined);
-  const [text, setText] = useState(undefined);
+  // const [name, setName] = useState(undefined);
+  // const [email, setEmail] = useState(undefined);
+  // const [text, setText] = useState(undefined);
 
-  const onSubmit = (event) => {
-    if (event) {
-      event.preventDefault(); //for preventing page default refresh
-      setName(event.target[0].value);
-      setEmail(event.target[1].value);
-      setText(event.target[2].value);
-    }
+  // const onSubmit = (event) => {
+  //   if (event) {
+  //     event.preventDefault(); //for preventing page default refresh
+  //     setName(event.target[0].value);
+  //     setEmail(event.target[1].value);
+  //     setText(event.target[2].value);
+  //   }
+  // };
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const [loading, setLoading] = useState(false);
+
+  const handleChange = (e) => {
+    const { target } = e;
+    const { name, value } = e.target;
+
+    setForm({
+      ...form,
+      [name]: value,
+    });
+    console.log(form.name);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    // service_urdngg8
+    // template_9wrgmbt
+    //  wOhdKmPxY4X7ikAPb
+    emailjs
+      .send(
+        "service_l2esei5",
+        "template_x9fkxb4",
+        {
+          from_name: form.name,
+          to_name: "Team Simplbyte",
+          from_email: form.email,
+          to_email: "team.simplbyte@gmail.com",
+          message: form.message,
+        },
+        "M3hfAJzpeeEbM_f1i"
+      )
+      .then(
+        () => {
+          setLoading(false);
+          alert("Thank you. I will get back to you as soon as possible.");
+
+          setForm({
+            name: "",
+            email: "",
+            message: "",
+          });
+        },
+        (error) => {
+          setLoading(false);
+          console.error(error);
+
+          alert("Ahh, something went wrong. Please try again.");
+        }
+      );
   };
 
   return (
     <>
-    <ContactHeader/>
+      <ContactHeader />
       <section className="form_section md:flex-col">
         <div>
           <div className="contact_form">
@@ -40,20 +100,26 @@ export const Contact = () => {
               icon={<HiMail fontSize="20px" />}
             />
           </div>
-          <form onSubmit={onSubmit} className="form">
+          <form ref={formRef} onSubmit={onSubmit} className="form">
             <div className="form_control">
               <label htmlFor="name">Name</label>
-              <input type="text" name="name" placeholder="Jhon Peter" />
+              <input type="text" name="name" placeholder="Jhon Peter" onChange={handleChange} />
             </div>
 
             <div className="form_control">
               <label htmlFor="email">Email</label>
-              <input type="text" name="email" placeholder="abc@example.com" />
+              <input type="text" name="email" placeholder="abc@example.com" onChange={handleChange} />
             </div>
 
             <div className="form_control">
-              <label htmlFor="text">Text</label>
-              <textarea className="border-2px" type="text" name="text" placeholder="Discription" />
+              <label htmlFor="email">Text</label>
+              <textarea
+                className="border-2px"
+                type="text"
+                name="message"
+                placeholder="Discription"
+                onChange={handleChange}
+              />
             </div>
             <div className="submit">
               <Button text="SUBMIT" />
@@ -70,5 +136,3 @@ export const Contact = () => {
     </>
   );
 };
-
-// export default Contact;
